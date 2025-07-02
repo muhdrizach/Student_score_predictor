1091ef79edcda46930e788c224640f1e7245cd37
Muhammed Riza <rizach234@gmail.com>
2025-07-02 21:02:08 +0530

    Update build.sh

--- .render/build.sh
+++ .render/build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-# Install Python 3.9 manually and set it as default
-echo "Setting up Python 3.9 manually"
+
+echo "⚙️  Forcing Python 3.9.13 via pyenv..."
 pyenv install 3.9.13 -s
 pyenv global 3.9.13
 
+python --version
 pip install --upgrade pip
 pip install -r requirements.txt
-
