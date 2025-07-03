6977c25c760edf538b297fb2f482c92700439722
Muhammed Riza <rizach234@gmail.com>
2025-07-03 14:05:27 +0530

    Update build.sh

--- .render/build.sh
+++ .render/build.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-
-echo "⚙️  Forcing Python 3.9.13 via pyenv..."
-pyenv install 3.9.13 -s
-pyenv global 3.9.13
-
-python --version
 pip install --upgrade pip
+pip install setuptools<58.0.0
 pip install -r requirements.txt
